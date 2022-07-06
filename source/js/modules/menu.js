17dff8434be50e69061c5de23f3d4f431428ebce
Zurida <undinka08@mail.ru>
2022-07-06 19:32:15 +0300

    Добавляет анимацию при переключении на экран призы

--- source/js/modules/menu.js
+++ source/js/modules/menu.js
@@ -2,6 +2,7 @@ export default () => {
   let header = document.querySelector(`.js-header`);
   let menuToggler = document.querySelector(`.js-menu-toggler`);
   let menuLinks = document.querySelectorAll(`.js-menu-link`);
+  const overlay = document.querySelector(`.overlay`);
 
   if (menuToggler) {
     menuToggler.addEventListener(`click`, function () {
@@ -16,7 +17,20 @@ export default () => {
   }
 
   for (let i = 0; i < menuLinks.length; i++) {
-    menuLinks[i].addEventListener(`click`, function () {
+    menuLinks[i].addEventListener(`click`, function (evt) {
+
+      if (menuLinks[i].dataset.href.includes(`prize`)) {
+        overlay.classList.add(`overlay--visible`);
+
+        evt.preventDefault();
+
+        setTimeout(() => {
+          window.location.href = `#${menuLinks[i].dataset.href}`;
+        }, 500);
+      } else {
+        overlay.classList.remove(`overlay--visible`);
+      }
+
       if (window.innerWidth < 1025) {
         header.classList.remove(`page-header--menu-opened`);
         document.body.classList.remove(`menu-opened`);
