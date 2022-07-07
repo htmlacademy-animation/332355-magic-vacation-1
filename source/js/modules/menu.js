0f53d6d65cf985452f422b484e5415c41c313b0b
Zurida <undinka08@mail.ru>
2022-07-07 15:35:38 +0300

    доработала анимацию только при переходе со страницы история

--- source/js/modules/menu.js
+++ source/js/modules/menu.js
@@ -19,7 +19,7 @@ export default () => {
   for (let i = 0; i < menuLinks.length; i++) {
     menuLinks[i].addEventListener(`click`, function (evt) {
 
-      if (menuLinks[i].dataset.href.includes(`prize`)) {
+      if (menuLinks[i].dataset.href.includes(`prize`) && window.location.href.includes(`story`)) {
         overlay.classList.add(`overlay--visible`);
 
         evt.preventDefault();
