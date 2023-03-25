93bf49241a2babc000e32c66ced0a0037ff2b15a
Zurida <undinka08@mail.ru>
2023-03-25 21:35:09 +0300

    added image observer

--- source/js/modules/page-switch-handler.js
+++ source/js/modules/page-switch-handler.js
@@ -1,14 +1,18 @@
 import AccentTypographyBuild from './accent-typography-builder';
+import observer from './observer';
 
 export default () => {
   document.querySelectorAll(`.title`).forEach((title) => {
     const text = new AccentTypographyBuild(title, 500, `active`, `transform`);
 
-    document.body.addEventListener(`screenChanged`, () => {
+    document.body.addEventListener(`screenChanged`, (evt) => {
       text.destroyAnimation();
       setTimeout(() => {
         text.runAnimation();
       }, 500);
+      if (evt.detail.screenName.includes(`prize`)) {
+        observer();
+      }
     });
   });
 };
