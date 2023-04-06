329ed6a3477bf6f465b9ec24ffe5fb3e7f7817ef
Zurida <undinka08@mail.ru>
2023-04-06 10:10:00 +0300

    настройки таймера

--- source/js/script.js
+++ source/js/script.js
@@ -25,3 +25,51 @@ pageSwitchHandler();
 
 const fullPageScroll = new FullPageScroll();
 fullPageScroll.init();
+
+const count = 5;
+let start = new Date();
+  // получаем время окончания таймера
+const stop = start.setMinutes(start.getMinutes() + count); 
+let previousTimeStamp;
+let done = false;
+const minute = document.querySelector('.minute');
+const second = document.querySelector('.second');
+
+
+function checkDate(timestamp) {
+  
+  if (start === undefined) {
+    start = timestamp;
+  }
+  // const elapsed = timestamp - start;
+  if (previousTimeStamp !== timestamp) {
+    const now = new Date().getTime();
+    let remain = stop - now;
+  
+    if (remain < 0) {
+      done = true 
+    }
+
+    if (done) {
+      window.cancelAnimationFrame(checkDate)
+    }
+
+
+    if (!done) {
+      second.textContent = Math.floor(remain / 1000) % 60
+      minute.textContent = Math.floor(remain / 1000 / 60) % 60 
+
+      window.requestAnimationFrame(checkDate);
+    }
+
+    // if (elapsed < 2000) {
+    //   // Stop the animation after 2 seconds
+    //   previousTimeStamp = timestamp;
+    //   if (!done) {
+    //     window.requestAnimationFrame(checkDate);
+    //   }
+    // }
+  }
+}
+
+requestAnimationFrame(checkDate)
